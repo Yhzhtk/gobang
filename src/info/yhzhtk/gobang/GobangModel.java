431db547c53527bbc33156da5573ee45aefce5c2
LSS1990 <liushuisheng@192.168.1.106>
2017-02-20 23:03:01 +0800

    add getAroundSituation func

--- src/info/yhzhtk/gobang/GobangModel.java
+++ src/info/yhzhtk/gobang/GobangModel.java
@@ -3,10 +3,17 @@ package info.yhzhtk.gobang;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 五子棋领域
+ * 
+ * @author gudh
+ * @create 2017年2月19日
+ */
 public class GobangModel {
 
+    // 棋盘大小
     public final static int BOARD_SIZE = 15;
-
+    // 棋盘情况
     private final PieceType[][] BOARD = new PieceType[BOARD_SIZE][BOARD_SIZE];
 
     public GobangModel() {
@@ -17,6 +24,15 @@ public class GobangModel {
         }
     }
 
+    /**
+     * 获取位置 loc 附近 distance 的所有可下节点
+     * 
+     * 
+     * @author gudh
+     * @param loc
+     * @param distance
+     * @return
+     */
     public List<Location> getNearLocation(Location loc, int distance) {
         if (distance < 1 || distance > BOARD_SIZE) {
             throw new RuntimeException("distance " + distance + " beyond limit");
@@ -29,6 +45,290 @@ public class GobangModel {
 
         return list;
     }
+
+    /**
+     * 是否可以下
+     * 
+     * @author gudh
+     * @param type
+     * @param loc
+     * @return
+     */
+    public boolean canPlay(Location loc) {
+        if (BOARD[loc.row][loc.col] != PieceType.EMPTY) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 下棋
+     * 
+     * @author gudh
+     * @param type
+     * @param loc
+     */
+    public void play(PieceType type, Location loc) {
+        if (type == PieceType.EMPTY) {
+            throw new RuntimeException(loc.toString() + " can't play empty");
+        }
+        if (!canPlay(loc)) {
+            throw new RuntimeException(loc.toString() + " not empty");
+        }
+        BOARD[loc.row][loc.col] = type;
+    }
+
+    /**
+     * 收回下棋
+     * 
+     * 
+     * @author gudh
+     * @param loc
+     */
+    public void replay(Location loc) {
+        if (BOARD[loc.row][loc.col] == PieceType.EMPTY) {
+            throw new RuntimeException("replay " + loc.toString() + " has be empty");
+        }
+        BOARD[loc.row][loc.col] = PieceType.EMPTY;
+    }
+
+    /**
+     * 判断棋谱是否满足某个条件
+     * 
+     * @author gudh
+     * @param type
+     * @param loc
+     * @param state
+     * @return
+     */
+    public boolean calculate(PieceType type, Location loc, PieceState state) {
+        switch (state) {
+        case COME5:
+            return toCOME5(type, loc);
+        case LIVE4:
+            return toLIVE4(type, loc);
+        case DOUBLE_DIE4:
+            return toDOUBLE_DIE4(type, loc);
+        case DIE4_LIVE3:
+            return toDIE4_LIVE3(type, loc);
+        case DOUBLE_LIVE3:
+            return toDOUBLE_LIVE3(type, loc);
+        case DIE3_LIVE3:
+            return toDIE3_LIVE3(type, loc);
+        case DIE4:
+            return toDIE4(type, loc);
+        case LIVE3:
+            return toLIVE3(type, loc);
+        case DIE3:
+            return toDIE3(type, loc);
+        case DOUBLE_LIVE2:
+            return toDOUBLE_LIVE2(type, loc);
+        case LIVE2:
+            return toLIVE2(type, loc);
+        case DIE2:
+            return toDIE2(type, loc);
+        }
+        throw new RuntimeException(state.name() + "not complete");
+    }
+
+    public static enum PieceType {
+        EMPTY, MACHINE, PEOPLE;
+
+        public PieceType next() {
+            if (this == MACHINE) {
+                return PEOPLE;
+            }
+            return MACHINE;
+        }
+    }
+
+    public static enum PieceState {
+        COME5, LIVE4, DOUBLE_DIE4, DIE4_LIVE3, DOUBLE_LIVE3, // 会赢
+        DIE3_LIVE3, DIE4, LIVE3, DIE3, DOUBLE_LIVE2, LIVE2, DIE2; // 待定
+    }
+
+    public static class Location {
+        private int row;
+        private int col;
+
+        public final static Location CENTER = new Location(BOARD_SIZE / 2, BOARD_SIZE / 2);
+        
+        public Location() {
+        	this.row = 0;
+        	this.col = 0;
+        }
+
+        public Location(int row, int col) {
+            if (row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE) {
+                throw new RuntimeException(row + ":" + col + " beyond limit " + BOARD_SIZE);
+            }
+            this.row = row;
+            this.col = col;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getCol() {
+            return col;
+        }
+        
+        public void setRow(int row) {
+			this.row = row;
+		}
+
+		public void setCol(int col) {
+			this.col = col;
+		}
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + col;
+            result = prime * result + row;
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Location other = (Location) obj;
+            if (col != other.col)
+                return false;
+            if (row != other.row)
+                return false;
+            return true;
+        }
+
+        public String toString() {
+            return row + ":" + col;
+        }
+    }
+    
+    public static enum Direction {
+    	LEFT_DIAGONAL,RIGHT_DIAGONAL,HORIZONTAL,VERTICAL
+    }
+    
+    public class ConnectSituation {
+    	private Location  startLoc;
+    	private Location  endLoc;
+    	private Direction direction;
+    	private boolean   connectState;
+    	private int       connectNumber;
+    	
+		public Location getStartLoc() {
+			return startLoc;
+		}
+		public void setStartLoc(Location startLoc) {
+			this.startLoc = startLoc;
+		}
+		public Location getEndLoc() {
+			return endLoc;
+		}
+		public void setEndLoc(Location endLoc) {
+			this.endLoc = endLoc;
+		}
+		public Direction getDirection() {
+			return direction;
+		}
+		public void setDirection(Direction direction) {
+			this.direction = direction;
+		}
+		public boolean getConnectState() {
+			return connectState;
+		}
+		public void setConnectState(boolean connectState) {
+			this.connectState = connectState;
+		}
+		public int getConnectNumber() {
+			return connectNumber;
+		}
+		public void setConnectNumber(int connectNumber) {
+			this.connectNumber = connectNumber;
+		}
+    }
+    
+    private ConnectSituation getAroundSituation(PieceType type,Location loc,
+    		Direction direction,int number) {
+    	int row = loc.row;
+    	int col = loc.col;
+    	ConnectSituation connectSituation = new ConnectSituation();
+    	
+    	if(BOARD[row][col] != PieceType.EMPTY) {
+    		connectSituation.setConnectState(false);
+    		return connectSituation;
+    	}
+    	
+    	int connectionNumber = 0;
+    	switch(direction) {
+    	case HORIZONTAL:
+        	while(col > 0 && BOARD[row][--col] == type)
+        		connectionNumber++;
+    		break;
+    	case VERTICAL:
+    	   	while(row > 0 && BOARD[--row][col] == type) 
+        		connectionNumber++;
+    		break;
+    	case LEFT_DIAGONAL:
+        	while(row > 0 && col > 0 && BOARD[--row][--col] == type)
+        		connectionNumber++;
+    		break;
+    	case RIGHT_DIAGONAL:
+        	while(row > 0 && col < (BOARD_SIZE - 1) && BOARD[--row][++col] == type)
+        		connectionNumber++;
+    		break;
+    	}
+    	
+    	Location startLoc = new Location();
+    	connectSituation.setStartLoc(startLoc);
+    	
+    	startLoc.setRow(row);
+    	startLoc.setCol(col);
+
+    	row = loc.row;
+    	col = loc.col;
+    	
+    	switch(direction) {
+    	case HORIZONTAL:
+        	while(col < (BOARD_SIZE - 1) && BOARD[row][++col] == type) 
+        		connectionNumber++;
+    		break;
+    	case VERTICAL:
+        	while(row < (BOARD_SIZE - 1) && BOARD[++row][col] == type)
+        		connectionNumber++;
+    		break;
+    	case LEFT_DIAGONAL:
+        	while(row < (BOARD_SIZE - 1) && col < (BOARD_SIZE - 1) && BOARD[++row][++col] == type)
+        		connectionNumber++;
+    		break;
+    	case RIGHT_DIAGONAL:
+        	while(row < (BOARD_SIZE - 1) && col > 0 && BOARD[++row][--col] == type)
+        		connectionNumber++;
+    		break;
+    	}
+
+    	Location endLoc = new Location();
+    	connectSituation.setEndLoc(endLoc);
+    	
+    	endLoc.setRow(row);
+    	endLoc.setCol(col);
+    	
+    	if(connectionNumber >= number) {
+    		connectSituation.setConnectState(true);
+    	} else {
+    		connectSituation.setConnectState(false);
+    	}
+    	
+    	return connectSituation;
+    }
+    
 
     private List<Location> getSingleNearLocation(Location loc, int distance) {
         List<Location> list = new ArrayList<Location>();
@@ -65,88 +365,49 @@ public class GobangModel {
         return list;
     }
 
-    public void play(PieceType type, Location loc) {
-        if (type == PieceType.EMPTY) {
-            throw new RuntimeException(loc.toString() + " can't play empty");
-        }
-        if (BOARD[loc.row][loc.col] != PieceType.EMPTY) {
-            throw new RuntimeException(loc.toString() + " not empty");
-        }
-        BOARD[loc.row][loc.col] = type;
-    }
-
-    public boolean calculate(PieceType type, Location loc, PieceState state) {
-        switch (state) {
-        case COME5:
-            return toCOME5(type, loc);
-        case LIVE4:
-            return toLIVE4(type, loc);
-        case DOUBLE_DIE4:
-            return toDOUBLE_DIE4(type, loc);
-        case DIE4_LIVE3:
-            return toDIE4_LIVE3(type, loc);
-        case DOUBLE_LIVE3:
-            return toDOUBLE_LIVE3(type, loc);
-        case DIE3_LIVE3:
-            return toDIE3_LIVE3(type, loc);
-        case DIE4:
-            return toDIE4(type, loc);
-        case LIVE3:
-            return toLIVE3(type, loc);
-        case DIE3:
-            return toDIE3(type, loc);
-        case DOUBLE_LIVE2:
-            return toDOUBLE_LIVE2(type, loc);
-        case LIVE2:
-            return toLIVE2(type, loc);
-        case DIE2:
-            return toDIE2(type, loc);
-        }
-        throw new RuntimeException(state.name() + "not complete");
-    }
-
-    public static enum PieceType {
-        EMPTY, MACHINE, PEOPLE
-    }
-
-    public static enum PieceState {
-        COME5, LIVE4, DOUBLE_DIE4, DIE4_LIVE3, DOUBLE_LIVE3, // 会赢
-        DIE3_LIVE3, DIE4, LIVE3, DIE3, DOUBLE_LIVE2, LIVE2, DIE2 // 待定
-    }
-
-    public static class Location {
-        private int row;
-        private int col;
-
-        public final static Location CENTER = new Location(BOARD_SIZE / 2, BOARD_SIZE / 2);
-
-        public Location(int row, int col) {
-            if (row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE) {
-                throw new RuntimeException(row + ":" + col + " beyond limit " + BOARD_SIZE);
-            }
-            this.row = row;
-            this.col = col;
-        }
-
-        public int getRow() {
-            return row;
-        }
-
-        public int getCol() {
-            return col;
-        }
-
-        public String toString() {
-            return row + ":" + col;
-        }
-    }
-
     private boolean toCOME5(PieceType type, Location loc) {
-        return true;
+    	for(Direction direction:Direction.values()) {
+        	ConnectSituation connectSituation = getAroundSituation(type,loc,direction,4);
+        	
+        	if(connectSituation.getConnectState()) {
+        		return true;
+        	}
+    	}
+        return false;
     }
 
     private boolean toLIVE4(PieceType type, Location loc) {
-        return true;
+    	
+    	for(Direction direction : Direction.values()) {
+    		ConnectSituation connectSituation = getAroundSituation(type,loc,direction,3);
+    		if(!connectSituation.getConnectState()) {
+    			continue;
+    		}
+    		
+    		Location startLoc = connectSituation.getStartLoc();
+    		Location prevLoc = new Location();
+    		
+    		Location endLoc = connectSituation.getEndLoc();
+    		Location nextLoc = new Location();
+    		
+    		switch(direction) {
+    		case HORIZONTAL:
+    			prevLoc.setRow(startLoc.getRow());
+    			prevLoc.setCol(startLoc.getCol() - 1);
+    			nextLoc.setRow(endLoc.getRow());
+    			nextLoc.setCol(endLoc.getCol() + 1);
+    			break;
+    		case VERTICAL:
+    			break;
+    		case LEFT_DIAGONAL:
+    			break;
+    		case RIGHT_DIAGONAL:
+    			break;
+    		}
+    		
+    	}
+    	
+        return false;
     }
 
     private boolean toDOUBLE_DIE4(PieceType type, Location loc) {
